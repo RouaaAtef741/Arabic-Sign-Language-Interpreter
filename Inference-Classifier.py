7d4539527b04e54eaab3f433f1831aa4aaf255e6
RouaaAtef741 <rouaaatef.22@gmail.com>
2024-07-15 17:13:24 +0300

    Update Inference-Classifier.py

--- Inference-Classifier.py
+++ Inference-Classifier.py
@@ -2,25 +2,30 @@ import pickle
 import cv2
 import mediapipe as mp
 import numpy as np
-import arabic_reshaper 
+import arabic_reshaper #from here is where the arabic text being displayed correctly libraries were imported. 
 from bidi.algorithm import get_display
 from PIL import Image, ImageDraw, ImageFont
 
+#open the model for prediction.
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
 
+#open the camera to capture the stream.
 cap = cv2.VideoCapture(0)
 
+#initialize mediapipe.
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
+#the labels now correspond with the arabic alphabet
 labels_dict = {0:'ا', 1:'ب', 2:'ت', 3:'ث', 4:'ج', 5:'ح', 6:'خ', 7:'د', 8:'ذ', 9:'ر', 10:'ز', 11:'س', 12:'ش', 
                13:'ص', 14:'ض', 15:'ط', 16:'ظ', 17:'ع', 18:'غ', 19:'ف', 20:'ق', 21:'ك', 22:'ل', 23:'م', 24:'ن', 25:'ه', 26:'و', 
                27:'ي', 28:'ة', 29:'ء'}
 
+#intialize the font path.
 font_path = "C:\\Users\\justa\\OneDrive\\Desktop\\Grad-proj\\Noto_Naskh_Arabic\\NotoNaskhArabic-VariableFont_wght.ttf"
 font = ImageFont.truetype(font_path, 32)
 
